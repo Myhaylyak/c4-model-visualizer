7b8a3cf07e92609f12ef9718885b556bb4ae69b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-19 13:44:09 +0000

    Deploy Myhaylyak/c4-model-visualizer to github.com/Myhaylyak/c4-model-visualizer.git:gh-pages

---
+++ precache-manifest.32aa2294dc47782976aabfd013f25940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fece04aef8fc996d6ebfca90db75416b",
+    "url": "/c4-model-visualizer/index.html"
+  },
+  {
+    "revision": "51fa89bfe4bfc8a5c700",
+    "url": "/c4-model-visualizer/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "44f5917b7d466eaaa7f3",
+    "url": "/c4-model-visualizer/static/css/main.75add5f5.chunk.css"
+  },
+  {
+    "revision": "51fa89bfe4bfc8a5c700",
+    "url": "/c4-model-visualizer/static/js/2.1f5f7a22.chunk.js"
+  },
+  {
+    "revision": "c7c6446d6a493238728623a60353428c",
+    "url": "/c4-model-visualizer/static/js/2.1f5f7a22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44f5917b7d466eaaa7f3",
+    "url": "/c4-model-visualizer/static/js/main.5addf09b.chunk.js"
+  },
+  {
+    "revision": "6fa84714aa12856fca87",
+    "url": "/c4-model-visualizer/static/js/runtime-main.9136dcd1.js"
+  }
+]);
